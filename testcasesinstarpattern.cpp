6fd0cc5a971b415999dd810febc43e86684e9a34
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-01-02 18:15:00 +0530

    Create testcasesinstarpattern.cpp

---
+++ testcasesinstarpattern.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+   int t;
+   cin>>t;
+   for(int test=0;test>t;t++)
+   {
+     for(int i=1;i<=n;i++)
+     {
+       for(int j=i;j>=i;j--)
+       {
+         cout<<"* ";
+       }
+       cout<<endl;
+     }
+   }
+ }
